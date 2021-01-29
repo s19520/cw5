59d3f390edbf5510aae3b57d47ed93f07471bcae
Klaudia Jakubowska <s19520@pjwstk.edu.pl>
2021-01-29 21:16:53 +0100

    cw6

---
+++ cw5/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using cw5.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw5.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        private WriteToFile _write = new WriteToFileImp();
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            httpContext.Request.EnableBuffering();
+            if (httpContext.Request != null)
+            {
+                string path = httpContext.Request.Path;
+                string method = httpContext.Request.Method;
+                string queryString = httpContext.Request.QueryString.ToString();
+                string body = "";
+                using(var reader=new StreamReader(httpContext.Request.Body, Encoding.UTF8, true, 1024, true))
+               {
+                    body = await reader.ReadToEndAsync();
+                    httpContext.Request.Body.Position = 0;
+                }
+                _write.Write(@".\requestsLog.txt", DateTime.Now.ToString());
+                _write.Write(@".\requestsLog.txt", "path: "+path);
+                _write.Write(@".\requestsLog.txt", "method: "+method);
+                _write.Write(@".\requestsLog.txt", "queryString: "+queryString);
+                _write.Write(@".\requestsLog.txt", "body: "+body);
+            }
+            if (_next !=null) await _next(httpContext);
+        }
+    }
+}
---
+++ cw5/Services/WriteToFileImp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw5.Services
+{
+    public class WriteToFileImp : WriteToFile
+    {
+        public void Write(string file, string text)
+        {
+
+            using (StreamWriter outputFile = new StreamWriter(file, true))
+            {
+                    outputFile.WriteLine(text+"\n");
+            }
+        }
+    }
+}
